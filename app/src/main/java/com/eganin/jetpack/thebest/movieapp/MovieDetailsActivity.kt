a4fdec2c3d4f463676af9a22b6c85433e192706a
Eganin <egorzaharin12@gmail.com>
2022-07-18 18:21:54 +0300

    add fragments

--- app/src/main/java/com/eganin/jetpack/thebest/movieapp/MovieDetailsActivity.kt
+++ app/src/main/java/com/eganin/jetpack/thebest/movieapp/MovieDetailsActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_detail)
+        setContentView(R.layout.fragment_movie_detail)
     }
 }
