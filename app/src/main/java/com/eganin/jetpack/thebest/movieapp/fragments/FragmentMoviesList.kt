89f41c8d3bfba1bda0c8c70452e9cbc115c88721
Eganin <egorzaharin12@gmail.com>
2022-07-24 11:13:25 +0300

    add navigation menu

--- app/src/main/java/com/eganin/jetpack/thebest/movieapp/fragments/FragmentMoviesList.kt
+++ app/src/main/java/com/eganin/jetpack/thebest/movieapp/fragments/FragmentMoviesList.kt
@@ -2,30 +2,38 @@ package com.eganin.jetpack.thebest.movieapp.fragments
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.eganin.jetpack.thebest.movieapp.R
 import com.eganin.jetpack.thebest.movieapp.adapters.MovieAdapter
+import com.eganin.jetpack.thebest.movieapp.data.models.Actor
+import com.eganin.jetpack.thebest.movieapp.data.models.Genre
 import com.eganin.jetpack.thebest.movieapp.data.models.Movie
 import com.eganin.jetpack.thebest.movieapp.data.models.loadMovies
 import com.eganin.jetpack.thebest.movieapp.databinding.FragmentMoviesListBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.*
+import kotlinx.parcelize.RawValue
 
 class FragmentMoviesList : Fragment() {
 
     private var _binding: FragmentMoviesListBinding? = null
     private val binding get() = _binding!!
 
+    private val superExceptionHandler = CoroutineExceptionHandler { _, exception ->
+        Log.e(TAG, "Failed", exception)
+    }
+
     private val movieAdapter = MovieAdapter()
-    private val uiScope = CoroutineScope(Dispatchers.Main)
+    private val uiScope = CoroutineScope(Dispatchers.Main + SupervisorJob() + superExceptionHandler)
     private var moviesData: List<Movie> = listOf()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,6 +46,7 @@ class FragmentMoviesList : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupUI()
         downloadData()
     }
 
@@ -67,6 +76,35 @@ class FragmentMoviesList : Fragment() {
         }
     }
 
+    //Plug
+    private fun setupUI() {
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.page_1 -> {
+                    true
+                }
+                R.id.page_2 -> {
+
+                    true
+                }
+                R.id.page_3 -> {
+
+                    true
+                }
+                R.id.page_4 -> {
+
+                    true
+                }
+                R.id.page_5 -> {
+
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
 
     private fun setupRecyclerView(dataMovies: List<Movie>) {
         binding.moviesRecyclerView.apply {
@@ -80,6 +118,9 @@ class FragmentMoviesList : Fragment() {
     }
 
     companion object {
+
+        private val TAG = "FRAGMENT_MOVIES_LIST"
+
         fun newInstance(columnCount: Int = DEFAULT_COLUMN_COUNT): FragmentMoviesList {
             val args = Bundle()
             args.putInt(COLUMN_COUNT_SAVE, columnCount)
